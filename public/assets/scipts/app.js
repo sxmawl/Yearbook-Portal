5f15963a7c9733ef9142a28e8157e158b7ae3445
Saksham Aggarwal <f20201508@pilani.bits-pilani.ac.in>
2021-04-18 23:52:39 +0530

    Profile section and navbar completed

--- public/assets/scipts/app.js
+++ public/assets/scipts/app.js
@@ -11,3 +11,18 @@ function signOut() {
 		console.log('User signed out.');
 	});
 }
+
+document.querySelector("input").addEventListener("click", ()=>{
+	for (let i = 20; i <= 32; i=i+0.1) {
+
+		if(i < 32){
+			document.querySelector('.search-bar').style.width = `${i}%`;
+		}
+		else{
+			return;
+		}
+		console.log("loops");
+
+	}
+	
+})
